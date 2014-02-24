94ef125753ffb815dfc34fb9ea50a5eb6efcaa59
Alexy Khabrov <deliverable@gmail.com>
2014-02-23 18:41:20 -0800

    initial commit

---
+++ EffinPickle.Core/App.fs
@@ -0,0 +1,12 @@
+﻿namespace EffinPickle.Core
+
+open System
+open Cirrious.CrossCore.IoC
+open EffinPickle.Core.ViewModels
+
+type App ()  = 
+    inherit Cirrious.MvvmCross.ViewModels.MvxApplication()
+    override u.Initialize() = 
+        u.RegisterAppStart<FirstViewModel>()
+
+    
